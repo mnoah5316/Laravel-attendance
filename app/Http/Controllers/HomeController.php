916c1dc72b787ecd8f1c305a13f5069bcfd774b6
Noah Mitchell <mnoah5316@gmail.com>
2025-08-28 20:35:04 -0500

    Eighth Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Present;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $present = Present::whereUserId(auth()->user()->id)->whereTanggal(date('Y-m-d'))->first();
+        $url = 'https://kalenderindonesia.com/api/'.config('absensi.api_key').'/libur/masehi/'.date('Y/m');
+        $kalender = file_get_contents($url);
+        $kalender = json_decode($kalender, true);
+        $libur = false;
+        $holiday = null;
+        if ($kalender['data'] != false) {
+            if ($kalender['data']['holidays']) {
+                foreach ($kalender['data']['holidays'] as $key => $value) {
+                    if ($value['masehi'] == date('Y-m-d')) {
+                        $holiday = $value['holiday']['name'];
+                        $libur = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return view('home', compact('present','libur','holiday'));
+    }
+}
